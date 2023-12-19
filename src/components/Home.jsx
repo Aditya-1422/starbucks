04633fb62b6088cee0c94ddc5cc06c3bb00ec86a
Aditya-1422 <adityaozalwarcnp@gmail.com>
2023-12-20 01:43:15 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/home.scss'
+
+const Home = () => {
+  return (
+    <div className='home' id=''>
+        <main>
+            <h1>Startbucks</h1>
+            <p>All type of Coffees......</p>
+        </main>
+    </div>
+  )
+}
+
+export default Home
